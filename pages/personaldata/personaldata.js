8dac11dece2edf1b17295f829ce27b1e51dbf547
wangwenhui992727 <378812352@qq.com>
2019-10-01 18:31:49 +0800

    第一次提交

---
+++ pages/personaldata/personaldata.js
@@ -0,0 +1,78 @@
+// pages/personaldata/personaldata.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */ 
+ 
+    onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
+    }
+  },
+
+  data: {
+    background: ['../img/e.jpg','../img/ajpg.jpg', '../img/b.jpg'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 4000,
+    duration: 500
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
